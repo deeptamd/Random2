615086317c47c5679b390efde5867b962c3ec2ff
deeptamd <deeptam_d@nid.edu>
2025-12-28 12:29:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,41 +12,28 @@ let daylightValue = 0;
 
 let daylightSlider, humiditySlider, skyConditionSlider;
 
-let murmurationSound, repelSound;
-let repelPoints = [];
-
-const MAX_BOIDS = 1200; // capped for performance
-const NEIGHBOR_DIST = 40;
-const DESIRED_SEP = 25;
-
-function preload() {
-  murmurationSound = loadSound("STARLINGS.mp3");
-  repelSound = loadSound("FLIGHT.mp3");
-}
+const MAX_BOIDS = 900; // reduced for performance
+const NEIGHBOR_DIST = 60;
+const DESIRED_SEP = 40;
 
 function setup() {
   createCanvas(1200, 600);
   pixelDensity(1);
 
   loadWeatherData();
-  setInterval(loadWeatherData, 15000);
+  setInterval(loadWeatherData, 20000);
 
   flock = new Flock();
 
   for (let i = 0; i < MAX_BOIDS; i++) {
     flock.addBoid(
       new Boid(
-        width / 2 + random(-50, 50),
-        height / 2 + random(-50, 50)
+        width / 2 + random(-100, 100),
+        height / 2 + random(-100, 100)
       )
     );
   }
 
-  setupUI();
-  murmurationSound.loop();
-}
-
-function setupUI() {
   daylightSlider = createSlider(0, 1, 0.5, 0.01);
   humiditySlider = createSlider(0, 100, 50, 1);
   skyConditionSlider = createSlider(0, 1, 0.5, 0.01);
@@ -69,29 +56,6 @@ function draw() {
   }
 
   flock.run(currentTemp, currentHumidity, daylightValue, skyVal);
-
-  if (repelPoints.length > 0) {
-    flock.repel(repelPoints);
-  }
-
-  adjustSound();
-}
-
-function mousePressed() {
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-    repelPoints.push(createVector(mouseX, mouseY));
-    repelSound.play(0, 1, 0.3, 0, 1);
-  }
-}
-
-function mouseReleased() {
-  repelPoints = [];
-}
-
-function adjustSound() {
-  let speed = flock.getAverageSpeed();
-  murmurationSound.rate(map(speed, 1.5, 4, 0.8, 1.3));
-  murmurationSound.setVolume(constrain(flock.boids.length / MAX_BOIDS, 0.3, 1));
 }
 
 function loadWeatherData() {
@@ -123,25 +87,6 @@ class Flock {
       b.run(this.boids);
     }
   }
-
-  repel(points) {
-    for (let p of points) {
-      for (let b of this.boids) {
-        let d = p5.Vector.dist(b.position, p);
-        if (d < 150) {
-          let force = p5.Vector.sub(b.position, p);
-          force.setMag(map(d, 0, 150, 0.6, 0));
-          b.applyForce(force);
-        }
-      }
-    }
-  }
-
-  getAverageSpeed() {
-    let sum = 0;
-    for (let b of this.boids) sum += b.velocity.mag();
-    return sum / this.boids.length;
-  }
 }
 
 // =======================
@@ -155,9 +100,10 @@ class Boid {
     this.acceleration = createVector(0, 0);
 
     this.maxSpeed = 3;
-    this.maxForce = 0.15;
+    this.maxForce = 0.18;
 
-    this.r = 4; // 🔥 BIGGER BOIDS
+    // 🔥🔥 VERY CLEAR, BIG BOIDS 🔥🔥
+    this.size = 10; // ← MAIN SIZE CONTROL (try 8–14)
   }
 
   run(boids) {
@@ -173,13 +119,12 @@ class Boid {
 
   applyEnvironment(temp, humidity, daylight, sky) {
     this.maxSpeed = map(daylight, 0, 1, 2, 4);
-
-    if (sky < 0.4) this.maxSpeed += 1;
-    if (humidity > 70) this.maxSpeed -= 0.5;
+    if (sky < 0.4) this.maxSpeed += 0.8;
+    if (humidity > 70) this.maxSpeed -= 0.4;
   }
 
   flock(boids) {
-    let sep = this.separate(boids).mult(1.6);
+    let sep = this.separate(boids).mult(1.8);
     let ali = this.align(boids).mult(1.0);
     let coh = this.cohesion(boids).mult(1.0);
 
@@ -197,25 +142,29 @@ class Boid {
 
   render() {
     let theta = this.velocity.heading() + PI / 2;
+
     fill(40);
-    noStroke();
+    stroke(40);
+    strokeWeight(0.5);
 
     push();
     translate(this.position.x, this.position.y);
     rotate(theta);
+
     beginShape();
-    vertex(0, -this.r * 2.5);
-    vertex(-this.r, this.r * 2);
-    vertex(this.r, this.r * 2);
+    vertex(0, -this.size * 2.2);          // nose
+    vertex(-this.size * 1.2, this.size);  // left wing
+    vertex(this.size * 1.2, this.size);   // right wing
     endShape(CLOSE);
+
     pop();
   }
 
   edges() {
-    if (this.position.x < 0) this.position.x = width;
-    if (this.position.y < 0) this.position.y = height;
-    if (this.position.x > width) this.position.x = 0;
-    if (this.position.y > height) this.position.y = 0;
+    if (this.position.x < -50) this.position.x = width + 50;
+    if (this.position.y < -50) this.position.y = height + 50;
+    if (this.position.x > width + 50) this.position.x = -50;
+    if (this.position.y > height + 50) this.position.y = -50;
   }
 
   separate(boids) {
